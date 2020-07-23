8fa30f50456b44d8321f771cfa374889aaad4c51
Marcio Machado <marcio_mach@hotmail.com>
2020-07-23 15:27:32 -0300

    Criado API 1 com o endpoint que retorna a taxa de juros

---
+++ SelecaoSoftplayer/TaxaJuros/Controllers/TaxaJurosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TaxaJuros.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaJurosController : ControllerBase
+    {
+
+        static readonly decimal TaxaJuro = 0.01M;
+
+        private readonly ILogger<TaxaJurosController> _logger;
+
+        public TaxaJurosController(ILogger<TaxaJurosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogDebug("iniciando endpoint que retorna a taxa de juro");
+            return Ok(TaxaJuro);
+        }
+    }
+}
